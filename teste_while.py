b34a4d1d1b3db6406703d21e9e8e77509e30b88a
Edvaldo-Paulo <108080295+Edvaldo-Paulo@users.noreply.github.com>
2024-03-27 15:46:05 -0300

    Update teste_while.py
    Alteração de um código de Login. O removi as variáveis nome e confirma_nome e o if que trabalhava com elas, e fiz pus no lugar as variáveis email e confirma_email, além de adicionar senha e confirma_senha.

--- teste_while.py
+++ teste_while.py
@@ -1,15 +1,25 @@
-nome = input('Informe seu nome: ')
-
-confirma_nome = input('Confirme seu nome: ')
-
+email = 'andrevieira00@gmail.com'
+senha = '1234'
+confirma_email = input('Confirme seu e-mail: ')
+confirma_senha = input('Confirme sua senha: ')
 
 contador = 1
+verificador = [confirma_email, confirma_senha]
 
 while contador <= 3:
-    if nome != confirma_nome:
+    if email != confirma_email:
         print('Você digitou o nome incorreto. Tente novamente!')
-        confirma_nome = input()
-        contador = contador + 1
+        confirma_email = input()
+        contador += 1
+    elif senha != confirma_senha:
+        print('Você digitou a senha incorreta. Tente novamente!')
+        confirma_senha = input()
+        contador += 1
+    elif verificador:
+        print('Você digitou o nome e senha incorretos. Tente novamente!')
+        confirma_email = input('Confirme o e-mail:')
+        confirma_senha = input('Confirme a senha: ')
+        contador += 1
     else:
         break
 
