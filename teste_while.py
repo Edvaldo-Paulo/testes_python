f2c9a7527afd9c2c246a8e4832b051112618cade
Edvaldo-Paulo <108080295+Edvaldo-Paulo@users.noreply.github.com>
2024-03-30 12:59:37 -0300

    Alteração feita no código para corrigir os erros. Agora ele funciona para o caso de errar apenas o e-mail ou a senha e para o caso de errar ambos. Seria mais ou menos o seguinte: Se o usuário errasse apenas um dos campos, o que está certo ficaria preenchido, e ele teria que reinserir apenas o que errou, no caso de errar ambos, teria que reinserir os dados de ambos os campos.

--- teste_while.py
+++ teste_while.py
@@ -4,22 +4,29 @@ confirma_email = input('Confirme seu e-mail: ')
 confirma_senha = input('Confirme sua senha: ')
 
 contador = 1
-verificador = [confirma_email, confirma_senha]
+
+
 
 while contador <= 3:
-    if email != confirma_email:
+    
+    if email != confirma_email and senha != confirma_senha:
         print('Você digitou o nome incorreto. Tente novamente!')
-        confirma_email = input()
-        contador += 1
-    elif senha != confirma_senha:
+        confirma_email = input('Confirme o e-mail: ')
         print('Você digitou a senha incorreta. Tente novamente!')
-        confirma_senha = input()
-        contador += 1
-    elif verificador:
-        print('Você digitou o nome e senha incorretos. Tente novamente!')
-        confirma_email = input('Confirme o e-mail:')
         confirma_senha = input('Confirme a senha: ')
         contador += 1
+
+    elif confirma_email != email or confirma_senha != senha:
+        if email != confirma_email:
+            print('Você digitou um e-mail incorreto. Tente novamente!')
+            confirma_email = input('Confirme o e-mail:')
+            contador += 1
+            
+        else:
+            print('Você digitou uma senha incorreta. Tente novamente')
+            confirma_senha = input('Confirme a senha: ')
+            contador += 1
+
     else:
         break
 
